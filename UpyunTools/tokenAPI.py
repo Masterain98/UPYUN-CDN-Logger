86b8ec1ff14aef6d6073c8816a4bb5f5a08c7e17
Masterain <i@irain.in>
2022-06-12 16:29:03 -0700

    upload token tools

---
+++ UpyunTools/tokenAPI.py
@@ -0,0 +1,29 @@
+import requests, random, string, time, json
+
+
+def get(username, password):
+    url = "https://api.upyun.com/oauth/tokens/"
+    code = "".join(random.choice(string.ascii_lowercase + string.ascii_uppercase +
+                                 string.digits) for i in range(32))
+    post_form = {
+        "username": username,
+        "password": password,
+        "code": code,
+        "name": "UPYUN_MYSQL_LOGGER",
+        "scope": "log",
+    }
+    req = requests.post(url, data=post_form)
+    try:
+        token = json.loads(req.text)["access_token"]
+        return token
+    except Exception as e:
+        print("error when gathering token")
+        print(e)
+        return None
+
+
+def delete(tokenName):
+    url = "https://api.upyun.com/oauth/tokens?name=%s"
+    result = requests.delete(url % tokenName)
+    print(result.text)
+
