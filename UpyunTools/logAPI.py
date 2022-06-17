09d2d074b6d44f380a4031fa4ce602bf4fc251da
Masterain <i@irain.in>
2022-06-16 23:37:30 -0700

    Update logAPI.py

--- UpyunTools/logAPI.py
+++ UpyunTools/logAPI.py
@@ -1,4 +1,6 @@
-import json, requests, gzip, os
+import json, requests, gzip, os, re
+from datetime import datetime
+from urllib.parse import urlparse
 
 
 def download(token, bucketName, domain, date):
@@ -24,3 +26,88 @@ def download(token, bucketName, domain, date):
         file_downloaded.append(gz_fileName.replace(".gz", ".log"))
     print("File downloaded: " + str(file_downloaded))
     return file_downloaded
+
+
+def EscapeLog(fileName):
+    result = []
+    for line in open(fileName):
+        split = line.split(' ')
+        # The length of split2 is more stable (len == 13)
+        split2 = line.split('"')
+
+        RemoteAddr = split[0]
+        RemoteUser = split[1] + " " + split[2]
+        TimeLocal = split[3].replace("[", "")
+        TimeLocal = str(datetime.strptime(TimeLocal, "%d/%b/%Y:%H:%M:%S"))
+        TimeZone = split[4].replace("]", "")
+        RequestMethod = split[5].replace('"', "")
+
+        url_path = urlparse(split[6])
+        Scheme = url_path.scheme
+        HTTPHost = url_path.hostname
+        Uri = url_path.path
+        QueryString = url_path.query
+        try:
+            QueryString2 = re.search(r"(\?|\!)(.)+$", Uri).group()
+            Uri = Uri.replace(QueryString2, "")
+        except:
+            QueryString2 = ""
+        QueryString = QueryString + QueryString2
+
+        ServerProtocol = split[7].replace('"', '')
+        HTTPStatus = split[8]
+        BodyBytesSent = split[9]
+        HTTPReferer = split[10].replace('"', '')
+
+        EdgeServerIP = split[-1].replace("\n", "")
+        RequestTime = split[-2]
+        CacheControl = split2[-2]
+        IsDynamic = split2[-3].replace(" ", "")
+        SourceCode = split2[-4]
+        if "hit" in split2[-5].lower():
+            CacheHit = "HIT"
+            RequestContentLength = split2[-5].replace("Hit", "").replace(" ","")
+        elif "miss" in split2[-5].lower():
+            CacheHit = "MISS"
+            RequestContentLength = split2[-5].replace("Miss", "").replace(" ", "")
+        else:
+            # Should not happen
+            CacheHit = "UNKNOWN"
+            RequestContentLength = split2[-5].replace(" ", "")
+        ContentType = split2[-6]
+        HTTPUserAgent = split2[-8]
+
+        this_log = {
+            "RemoteAddr": RemoteAddr,
+            "RemoteUser": RemoteUser,
+            "TimeLocal": TimeLocal,
+            "TimeZone": TimeZone,
+            "RequestMethod": RequestMethod,
+            "Scheme": Scheme,
+            "HTTPHost": str(HTTPHost),
+            "QueryString": QueryString,
+            "Uri": Uri,
+            "ServerProtocol":ServerProtocol,
+            "HTTPStatus": HTTPStatus,
+            "BodyBytesSent": BodyBytesSent,
+            "HTTPReferer": HTTPReferer,
+            "HTTPUserAgent": HTTPUserAgent,
+            "ContentType": ContentType,
+            "RequestContentLength": RequestContentLength,
+            "CacheHit": CacheHit,
+            "SourceCode": SourceCode,
+            "IsDynamic": IsDynamic,
+            "CacheControl": CacheControl,
+            "RequestTime": RequestTime,
+            "EdgeServerIP": EdgeServerIP
+        }
+        result.append(this_log)
+    return result
+
+
+def SaveSQL(fileList):
+    for file in fileList:
+        print(file)
+
+
+EscapeLog("../data/17_00-blogpic.irain.in-8420802881497378793.log")
